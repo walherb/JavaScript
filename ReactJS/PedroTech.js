fa03234a495d5ebed4076332f016bf4ce3c623b4
Walusimbi Herbert Henry <walherb@gmail.com>
2023-04-18 00:52:57 +0300

    Added PedroTech.js

---
+++ ReactJS/PedroTech.js
@@ -0,0 +1,52 @@
+/*
+! https://react.dev/learn
+!5 Pro-Level React Do's & Don'ts
+!How to use ChatGPT To Write Better React Code
+!React Hooks Course - All React Hooks Explained
+
+!- 3 Alternatives for Heroku's Free Tier - Full Stack & API Hosting
+render.com
+railway.app
+cyclic.sh  --> { https://overalls-seagull.cyclic.app } /api/products
+
+! KEY TOPICS
+- Components & Props 
+- States 
+- Hooks 
+- Redux
+- TypeScript 
+- Forms 
+- State Management 
+- Firebase Project 
+
+! React Course For Beginners - Learn React in 8 Hours
+! REACT COURSE FULL TIMESTAMPS
+00:00:00 |  Introduction
+! 00:07:52 |  Module 1: What is React?
+  npx create-react-app . 
+! 00:18:14 |  Module 2: JSX, Components, Props
+00:38:02 |  Module 3: Ternary Operators, Lists and CSS
+const planets = [
+  { name: "Mars", isGasPlanet: false },
+  { name: "Earth", isGasPlanet: false },
+  { name: "Jupiter", isGasPlanet: true },
+  { name: "Venus", isGasPlanet: false },
+  { name: "Neptune", isGasPlanet: true },
+  { name: "Uranus", isGasPlanet: true },
+];
+01:04:33 |  Module 4: States in React, useState Hook
+01:30:39 |  Module 5: CRUD in React, ToDo List
+02:10:08 |  Module 6: Component Lifecycle, useEffect Hook
+02:28:16 |  Module 7: Fetching Data from API's 
+02:57:09 |  Module 8: React Router DOM
+03:13:03 |  Module 9: State Management, useContext Hook
+03:32:02 |  Module 10: React Query
+03:51:38 |  Module 11: Forms in React
+04:19:01 |  Module 12: Custom Hooks
+04:40:27 |  Module 13: Typescript, Type Safety
+05:06:12 |  Module 14: Redux Toolkit
+05:29:29 |  Module 15: Firebase Project (Part 1)
+06:05:40 |  Module 16: Firebase Project (Part 2)
+06:46:08 |  Module 17: Firebase Project (Part 3)
+07:41:58 |  Module 18: Deploying a Firebase React App
+*/
