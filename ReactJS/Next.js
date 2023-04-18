1c7e995d44152a780630735f96651542d95ce1be
Walusimbi Herbert Henry <walherb@gmail.com>
2023-04-18 20:07:30 +0300

    Next.js

---
+++ ReactJS/Next.js
@@ -0,0 +1,36 @@
+//! Next.js Crash Course - Server Side React - YouTube {Traversy Media}
+href = 'https://bootswatch.com/5/cerulean/bootstrap.min.css';
+// npx @next/codemod new-link .
+// Minimalistic framework for redering React applications on the server
+
+// ------------ Core Features of Next ------------------------
+/*
+- Server-rendered React apps 
+- Automatic code splitting 
+- Simple page based routing
+- Built in CSS support  
+- Hot Reloading
+- Deployment
+
+npm init 
+npm install next react react-dom 
+{
+    "scripts": {
+        "dev": "next",
+        "build": "next build",
+        "start": "next start"
+    }
+}
+pages/index.js 
+export default () => <div>Hello World</div>
+
+Link Module 
+Import Link from 'next/link'
+
+<Link href="/"><a>Home</a></Link>
+
+<Link href="/about"><button>About</button></Link>
+
+*/
+
+//! Next.js 13 Crash Course - App Directory, React Server Components & More {: Brad}
